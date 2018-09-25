d4a9a17cbc5e83cf4f055b08910bc5480039cdee
Nicole <polin982@newschool.edu>
2018-09-25 12:12:44 -0400

    Unity

---
+++ minigame/Assets/Script/DestroyByContact.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+
+{
+
+
+    private GameController gameController;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+
+        if (other.tag == "Player")
+        {
+            return;
+
+           
+        }
+     
+
+    }
+}
+
---
+++ minigame/Assets/Script/GameCollectable.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCollectable : MonoBehaviour
+{
+
+    public float speed;
+    public Vector3 spawnValues;
+    public int collectableCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public GameObject collectable;
+   // public GameObject OnTriggerEnter;
+
+    //private Rigidbody rb;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < collectableCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(collectable, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            //rb.AddForce(movement * speed);
+        }
+    }
+}
+
+       // void OnTriggerEnter(Collider other)
+       // {
+     //       if (other.gameObject.CompareTag("Pick Up"))
+     //       {
+     //           other.gameObject.SetActive(false);
+          //  }
+     //   }
+   // }
+//}
+
+
+ 
